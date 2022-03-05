41271dcf5da77f3244fa4d9adcbd3dc636417614
Sriram <sriramsonaganti@gmail.com>
2022-03-05 10:21:30 +0530

    initial commit

---
+++ DBSBackend/src/main/java/com/backend/DBSBackend/DbsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.DBSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsBackendApplication.class, args);
+	}
+
+}
